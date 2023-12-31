245466f9249aa9f3b01a6f84574ad98072aba593
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-12-31 12:06:44 +0600

    first commit

---
+++ date_and_time.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Today is ".date("d/m/Y")."<br/>";
+    echo "Today is ".date("l")."<br/>";
+    date_default_timezone_set('Asia/Dhaka');
+    echo "Bangladesh Time is ".date("h:i:sa");
+?>
