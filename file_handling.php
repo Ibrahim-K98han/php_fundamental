b3e5043d5a84474a85c99aaaa0fd8ffaa6a2d36b
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-12-31 18:35:13 +0600

    file read and close added

---
+++ file_handling.php
@@ -0,0 +1,26 @@
+<?php
+  //  echo readfile("text.txt"); //file read
+
+  // read file
+    /*$ourfile = fopen("text.txt","r") or die("File not found !!");
+    echo fread($ourfile, filesize("text.txt"));
+    fclose($ourfile);*/
+
+      // first line print
+     /* $ourfile = fopen("text.txt","r") or die("File not found !!");
+      echo fgets($ourfile, filesize("text.txt"));
+      fclose($ourfile);*/
+
+        // first char print
+        /*$ourfile = fopen("text.txt","r") or die("File not found !!");
+        echo fgetc($ourfile);
+        fclose($ourfile);*/
+
+        // read all line
+      $ourfile = fopen("text.txt","r") or die("File not found !!");
+      while(!feof($ourfile)){
+        echo fgets($ourfile)."<br>";
+      }
+     
+      fclose($ourfile);
+?>
