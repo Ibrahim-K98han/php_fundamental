ec449faa718e96db8f23cda3aa00e14cb52c6df5
Ibrahim Khan <aitvetibrahim@gmail.com>
2024-01-01 11:03:26 +0600

    image uploaded done

--- file_handling.php
+++ file_handling.php
@@ -26,42 +26,37 @@
 
 
       // Create File
-      $createFile = fopen("new.txt","w") or die("File not Found !!");
+     /* $createFile = fopen("new.txt","w") or die("File not Found !!");
       $one = "Ibrahim Khan";
-      fwrite($createFile, $one);
+      fwrite($createFile, $one);*/
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      if(isset($_FILES['image'])){
+        $filename = $_FILES['image']['name'];
+        $filetmp = $_FILES['image']['tmp_name'];
+        move_uploaded_file($filetmp, "images/".$filename);
+        echo "Image upload successfully";
+      }
 
 
 
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="" method="POST" enctype="multipart/form-data">
+      <input type="file" name="image" id="">
+      <input type="submit" Value="Submit">
+  </form>
+</body>
+</html>
